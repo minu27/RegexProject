dc1e175216d3ad9a8d3c64ede9720aee8a23cc7d
minu27 <minal.vaity95@gmail.com>
2022-09-25 22:01:41 -0500

    Add length weakness

---
+++ script.js
@@ -0,0 +1,43 @@
+const strengthMeter = document.getElementById('strength-meter')
+const passwordInput = document.getElementById('password-input')
+const reasonsContainer = document.getElementById('reasons')
+
+passwordInput.addEventListener('input', updateStrengthMeter)
+updateStrengthMeter()
+
+function updateStrengthMeter() {
+    const weaknesses = calculatePasswordStrength(passwordInput.value)
+    let strength = 100
+    reasonsContainer.innerHTML = ''
+    weaknesses.forEach(weakness => {
+        if(weakness == null) return
+        strength -= weakness.deduction
+        const messageElement = document.createElement('div')
+        messageElement.innerText = weakness.message
+        reasonsContainer.appendChild(messageElement)
+    })
+    strengthMeter.style.setProperty('--strength', strength)
+}
+function calculatePasswordStrength(password) {
+    const weaknesses = []
+    weaknesses.push(lengthWeakness(password))
+    return weaknesses
+}
+
+function lengthWeakness(password) {
+    const length = password.length
+
+    if(length <= 5){
+        return {
+            message: 'Your password is too short',
+            deduction: 40
+        }
+    }
+
+    if(length <= 10){
+        return {
+            message: 'Your password could be longer',
+            deduction: 15
+        }
+    }    
+}
